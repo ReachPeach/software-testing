1e9ab7f729d0e7db0fb5306e42eb51643af6ca3e
ReachPeach <maxim.busyuk@gmail.com>
2022-04-09 05:08:32 +0300

    selenide tests workaround

---
+++ selenide/src/main/java/pages/AuthorPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class AuthorPage {
+    public static By authorHeader = By.className("App-header");
+
+    public static By authorName = By.className("author-name");
+
+    public static By songsNames = By.className("Song-name");
+}
